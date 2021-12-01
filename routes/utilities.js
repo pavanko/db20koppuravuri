6842a79aa0f9a21668de1f9a4a43437546d21d80
pavanko <pavankumarkoppuravuri1999@gmail.com>
2021-11-30 21:14:54 -0600

    added bonus part

--- routes/utilities.js
+++ routes/utilities.js
@@ -18,11 +18,11 @@ router.get('/', utility_controlers.utility_view_all_Page );
 // GET request for one utility. 
 router.get('/utility/:id', utility_controlers.utility_detail); 
 /* GET detail utility page */ 
-router.get('/detail', utility_controlers.utility_view_one_Page); 
+router.get('/detail',secured, utility_controlers.utility_view_one_Page); 
 /* GET create utility page */ 
 router.get('/create', utility_controlers.utility_create_Page); 
 /* GET create update page */ 
 router.get('/update',secured, utility_controlers.utility_update_Page); 
 /* GET create costume page */ 
-router.get('/delete', utility_controlers.utility_delete_Page); 
+router.get('/delete',secured, utility_controlers.utility_delete_Page); 
 module.exports = router;
