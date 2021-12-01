d67b1bda6644dfb051c292a4d968befdbf4331d9
pavanko <pavankumarkoppuravuri1999@gmail.com>
2021-11-30 21:11:11 -0600

    added upto part 5

--- routes/utilities.js
+++ routes/utilities.js
@@ -3,6 +3,14 @@ const utility_controlers= require('../controllers/utility');
 var router = express.Router();
 
 /* GET utility */
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
+ 
 
 router.get('/', utility_controlers.utility_view_all_Page );
 
@@ -14,7 +22,7 @@ router.get('/detail', utility_controlers.utility_view_one_Page);
 /* GET create utility page */ 
 router.get('/create', utility_controlers.utility_create_Page); 
 /* GET create update page */ 
-router.get('/update', utility_controlers.utility_update_Page); 
+router.get('/update',secured, utility_controlers.utility_update_Page); 
 /* GET create costume page */ 
 router.get('/delete', utility_controlers.utility_delete_Page); 
 module.exports = router;
